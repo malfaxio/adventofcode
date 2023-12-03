1a0f9e2853271fd925ba2d2d83bbb8d80d40cc84
MalfaX <malfax@gmail.com>
2023-12-04 00:46:52 +0100

    day 02 added

---
+++ 2023/02/day02p1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+
+sys.path.append('../../libs')
+
+import utils
+import re
+
+lines = utils.aoc_read("fs.txt")
+
+red_limit = 12
+green_limit = 13
+blue_limit = 14
+
+s = 0
+for l in lines:
+    m = re.match('^Game (\d+)', l)
+    gid = int(m.group(1))
+
+    red = [int(x) for x in re.findall('(\d+) red', l)]
+    green = [int(x) for x in re.findall('(\d+) green', l)]
+    blue = [int(x) for x in re.findall('(\d+) blue', l)]
+
+    if (max(red) <= red_limit) and (int(max(green)) <= green_limit) and (int(max(blue)) <= blue_limit):
+        s += gid
+
+
+print(s)
