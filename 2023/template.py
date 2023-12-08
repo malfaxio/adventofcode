6fa5498893138ad099d8d7f0cff59e0b68b61789
MalfaX <malfax@gmail.com>
2023-12-08 10:20:07 +0100

    stuff

--- 2023/template.py
+++ 2023/template.py
@@ -6,6 +6,8 @@ sys.path.append('../libs')
 
 import utils
 
-l = utils.aoc_read_bysection("test.txt", convert="int")
 
-print(l)
+if __name__ == '__main__':
+    l = utils.aoc_read_bysection("test.txt", convert="int")
+
+    print(l)
